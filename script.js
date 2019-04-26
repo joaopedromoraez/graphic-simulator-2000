8786b2d2526e74537a0a0df73f19f1efe0f7d227
JoaoPedroMoraes <joaopedromoraes@outlook.com>
2019-04-26 16:04:23 -0300

    add bezier cubic

--- script.js
+++ script.js
@@ -121,14 +121,30 @@ function circulo(xCenter, yCenter, radius) {
 }
 
 //Curva de Belzie
-function bezier(x1, y1, x2, y2, x3, y3) {
-    console.log(`x1(${x1},${y1})`);
-    console.log(`x2(${x2},${y2})`);
-    console.log(`x3(${x3},${y3})`);
-    for(let t = 0 ; t <= 1 ; t += 0.01) 
+function bezierQuadratic(x1, y1, x2, y2, x3, y3) {
+    console.log(`P1(${x1},${y1})`);
+    console.log(`P2(${x2},${y2})`);
+    console.log(`P3(${x3},${y3})`);
+    for(let t = 0 ; t <= 1 ; t += 0.001) 
     { 
         let xu = Math.pow(1-t,2)*x1 + 2*t*(1-t)*x2 + Math.pow(t,2)*x3; 
         let yu = Math.pow(1-t,2)*y1 + 2*t*(1-t)*y2 + Math.pow(t,2)*y3; 
+        
+        console.table(Math.round(xu) , Math.round(yu)) ; 
+        pixelPaint(Math.round(xu) , Math.round(yu)) ; 
+    } 
+} 
+
+function bezierCubic(x1, y1, x2, y2, x3, y3,x4,y4) {
+    console.log(`P1(${x1},${y1})`);
+    console.log(`P2(${x2},${y2})`);
+    console.log(`P3(${x3},${y3})`);
+    console.log(`P4(${x4},${y4})`);
+    for(let t = 0 ; t <= 1 ; t += 0.001) 
+    { 
+        let xu = Math.pow(1-t,3)*x1 + 3*t*Math.pow(1-t,2)*x2 + 3*Math.pow(t,2)*(1-t)*x3 + Math.pow(t,3)*x4;
+
+        let yu = Math.pow(1-t,3)*y1 + 3*t*Math.pow(1-t,2)*y2 + 3*Math.pow(t,2)*(1-t)*y3 + Math.pow(t,3)*y4;
         
         console.table(Math.round(xu) , Math.round(yu)) ; 
         pixelPaint(Math.round(xu) , Math.round(yu)) ; 
