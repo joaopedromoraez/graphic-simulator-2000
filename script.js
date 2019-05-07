7e9cf2916be8f463b73e1dc5b77889139e41dfa1
JoaoPedroMoraes <joaopedromoraes@outlook.com>
2019-05-07 19:47:07 -0300

    addition of rope to the bezier algorithm

--- script.js
+++ script.js
@@ -151,30 +151,37 @@ function circle(xCenter, yCenter, radius) {
 //Curva de Bezier
 //Bezier Quadratico
 function bezierQuadratic(x1, y1, x2, y2, x3, y3) {
+    let pontosbezier = [];
     console.log(`P1(${x1},${y1})`);
     console.log(`P2(${x2},${y2})`);
     console.log(`P3(${x3},${y3})`);
-    for (let t = 0; t <= 1; t += 0.001) {
+    for (let t = 0; t <= 1; t += 0.1) {
         let xu = Math.pow(1 - t, 2) * x1 + 2 * t * (1 - t) * x2 + Math.pow(t, 2) * x3;
         let yu = Math.pow(1 - t, 2) * y1 + 2 * t * (1 - t) * y2 + Math.pow(t, 2) * y3;
-
-        console.table(Math.round(xu), Math.round(yu));
-        pixelPaint(Math.round(xu), Math.round(yu));
+        pontosbezier.push([Math.round(xu), Math.round(yu)]);
     }
+    for (let x = 0; x < (pontosbezier.length - 1); x++){
+        bresenham(pontosbezier[x][0],pontosbezier[x][1],pontosbezier[x+1][0],pontosbezier[x+1][1],);
+    }
+    console.table(pontosbezier);
 }
 
 //Bezier Cubico
 function bezierCubic(x1, y1, x2, y2, x3, y3, x4, y4) {
+    let pontosbezier = [];
     console.log(`P1(${x1},${y1})`);
     console.log(`P2(${x2},${y2})`);
     console.log(`P3(${x3},${y3})`);
     console.log(`P4(${x4},${y4})`);
-    for (let t = 0; t <= 1; t += 0.001) {
+    for (let t = 0; t <= 1; t += 0.1) {
         let xu = Math.pow(1 - t, 3) * x1 + 3 * t * Math.pow(1 - t, 2) * x2 + 3 * Math.pow(t, 2) * (1 - t) * x3 + Math.pow(t, 3) * x4;
         let yu = Math.pow(1 - t, 3) * y1 + 3 * t * Math.pow(1 - t, 2) * y2 + 3 * Math.pow(t, 2) * (1 - t) * y3 + Math.pow(t, 3) * y4;
-        console.table(Math.round(xu), Math.round(yu));
-        pixelPaint(Math.round(xu), Math.round(yu));
+        pontosbezier.push([Math.round(xu), Math.round(yu)]);
     }
+    for (let x = 0; x < (pontosbezier.length - 1); x++){
+        bresenham(pontosbezier[x][0],pontosbezier[x][1],pontosbezier[x+1][0],pontosbezier[x+1][1],);
+    }
+    console.table(pontosbezier);
 }
 
 //Poligono
